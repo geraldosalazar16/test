d344447da4fbe05042397636a2d9ea08230bb4ad
geraldosalazar16 <34693854+geraldosalazar16@users.noreply.github.com>
2020-11-26 00:22:20 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,97 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
+const { User } = require('../db');
+const db = require('../db');
+const user = require('../db/models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await db.User.findAll();
+        res.json(users)
+    } catch (error) {
+        handleError(res, error)
+    }
 });
 
-router.post('/', function(req, res, next) {
-  
+router.post('/', 
+[
+    body('first_name').notEmpty(),
+    body('last_name').notEmpty(),
+    body('birthday').isDate(),
+    body('password').notEmpty(),
+    body('gender').notEmpty(),
+],
+async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const resultado = await db.User.create(req.body)
+        res.json({
+            id: resultado.id
+        })
+    } catch (error) {
+        handleError(error)
+    }
 });
 
-router.put('/', function(req, res, next) {
-  
+router.put('/:id', 
+[
+    body('first_name').notEmpty(),
+    body('last_name').notEmpty(),
+    body('birthday').isDate(),
+    body('password').notEmpty(),
+    body('gender').notEmpty(),
+],
+async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const id = req.params.id;
+    try {
+        const currentValue = db.User.findByPk(id)
+        if (currentValue === null) {
+            res.status(404).send(`User with Id ${id} not found`)
+        } else {
+            Object.keys(currentValue).forEach(key => {
+                if (currentValue[key] !== req.body[key]) {
+                    currentValue[key] = req.body[key]
+                }
+            })
+            await currentValue.save();
+            res.json()
+        } 
+    } catch (error) {
+        handleError(error)
+    }
 });
 
-router.delete('/', function(req, res, next) {
-  
+router.delete('/:id', 
+async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = db.User.findByPk(id)
+        if (user === null) {
+            res.status(404).send(`User with Id ${id} not found`)
+        } else {
+            await currentValue.destroy();
+            res.json()
+        } 
+    } catch (error) {
+        handleError(error)
+    }
 });
+
+const handleError = (res, error) => {
+    console.log(error)
+    res.status(500).json({
+        error
+    })
+}
 
 module.exports = router;
